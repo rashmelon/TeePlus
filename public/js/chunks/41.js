cc9e9a770b1aff95c358a6e879f6f9e8b3d19a92
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2020-08-16 00:16:21 +0200

    login errors fix

--- public/js/chunks/41.js
+++ public/js/chunks/41.js
@@ -9,6 +9,14 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 //
 //
 //
@@ -97,18 +105,22 @@ __webpack_require__.r(__webpack_exports__);
           password: this.password
         }
       };
-      this.$store.dispatch('auth/loginJWT', payload).then(function () {
-        _this.$vs.loading.close();
-      }).catch(function (error) {
-        console.log(error.response.data.error);
+      this.$store.dispatch('auth/loginJWT', payload).then(function () {}).catch(function (error) {
+        for (var _i = 0, _Object$entries = Object.entries(error.response.data.errors); _i < _Object$entries.length; _i++) {
+          var _Object$entries$_i = _slicedToArray(_Object$entries[_i], 2),
+              key = _Object$entries$_i[0],
+              value = _Object$entries$_i[1];
 
-        _this.$vs.notify({
-          title: 'Error',
-          text: error.response.data.error,
-          iconPack: 'feather',
-          icon: 'icon-alert-circle',
-          color: 'danger'
-        });
+          _this.$vs.notify({
+            title: key,
+            text: value[0],
+            iconPack: 'feather',
+            icon: 'icon-alert-circle',
+            color: 'danger'
+          });
+        }
+      }).then(function () {
+        _this.$vs.loading.close();
       });
     }
   }
